4f62036db77b9ad31037a9236c8886a4af5264d6
Jason Anderson <jas.nanders.n@gmail.com>
2021-04-16 21:46:24 -0700

    added connection script, started building schema and index script

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+
+);
+
+CREATE TABLE role (
+
+);
+
+CREATE TABLE employee (
+
+);
---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const connection = require("./config/connection");
+
+const init = () => {
+    inquirer.prompt({
+        
+    })
+};
