2d3880ba78438c6002248930e845a4f417a7ea49
Jason Anderson <jas.nanders.n@gmail.com>
2021-04-16 23:26:05 -0700

    added content to index.js and schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,30 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-
+id INT NOT NULL AUTO_INCREMENT,
+department_name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10, 2) NOT NULL,
+department_id INT,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
 );
+
+INSERT INTO department (department_name) VALUES
+
+INSERT INTO roles (title, salary, department_id) VALUES
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES
--- index.js
+++ index.js
@@ -1,8 +1,81 @@
 const inquirer = require("inquirer");
 const connection = require("./config/connection");
+const consoleTable = require("console.table");
 
 const init = () => {
     inquirer.prompt({
-        
-    })
+        type: "list",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+            "Add department",
+            "Add role",
+            "Add employee",
+            "View departments",
+            "View roles",
+            "View employees",
+            "Update employee roles",
+            "Exit"
+        ]
+    }).then(response => {
+        switch (response.choices) {
+            case "Add department":
+                addDepartment();
+                break;
+            case "Add role":
+                addRole();
+                break;
+            case "Add employee":
+                addEmployee();
+                break;
+            case "View departments":
+                viewDepartments();
+                break;
+            case "View roles":
+                viewRoles();
+                break;
+            case "View employees":
+                viewEmployees();
+                break;
+            case "Update employee roles":
+                updateEmployeeRoles();
+                break;
+            case "Exit":
+                connection.end();
+                break;
+        };
+    });
 };
+
+const addDepartment = () => {
+
+};
+
+const addRole = () => {
+    
+};
+
+const addEmployee = () => {
+    
+};
+
+const viewDepartments = () => {
+    
+};
+
+const viewRoles = () => {
+    
+};
+
+const viewEmployees = () => {
+    
+};
+
+const updateEmployeeRoles = () => {
+
+};
+
+connection.connect(err => {
+    if (err) throw err;
+    init();
+});
